be5ff5e5f80b6837c6839d65ad66f8089fae5ddf
Satyajit Sial <SatyajitS3@verifone.com>
2023-09-27 19:43:54 +0530

    changing the plugin name

--- kong-py-plugins/AI-Powered-Analytics.py
+++ kong-py-plugins/AI-Powered-Analytics.py
@@ -65,4 +65,4 @@ class Plugin(object):
 # add below section to allow this plugin optionally be running in a dedicated process
 if __name__ == "__main__":
     from kong_pdk.cli import start_dedicated_server
-    start_dedicated_server("py-ai-analytics", Plugin, version, priority, Schema)
+    start_dedicated_server("AI-Powered-Analytics", Plugin, version, priority, Schema)
