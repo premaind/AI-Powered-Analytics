1813955b880d5759c9774b012554ff4347d74731
Satyajit Sial <SatyajitS3@verifone.com>
2023-09-27 18:02:43 +0530

    first commit

---
+++ AI-Powered-Analytics.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import os
+import sys
+import kong_pdk.pdk.kong as kong
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from langchain import OpenAI
+from IPython.display import Markdown, display
+# from flask import Flask, request, jsonify
+
+Schema = (
+    {"OPENAI_API_KEY": {"type": "string"}},
+    {"model_name": {"type": "string"}},
+)
+
+version = '0.1.0'
+priority = 0
+
+# This is an example plugin that uses Python Geocoding
+
+class Plugin(object):
+    def __init__(self, config):
+        self.config = config
+
+    def access(self, kong: kong.kong):
+        kong.log.info("##########################  Inside Access Phase ######################################")
+        os.environ["OPENAI_API_KEY"] = self.config['OPENAI_API_KEY']
+        directory_path = "/tmp/analytics"
+        max_input_size = 4096
+        # set number of output tokens
+        num_outputs = 2000
+        # set maximum chunk overlap
+        max_chunk_overlap = 20
+        # set chunk size limit
+        chunk_size_limit = 600
+
+
+        # define prompt helper
+        prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+        kong.log.info("########################## prompt_helper ##########################")
+        
+        # define LLM
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name=self.config['model_name'], max_tokens=num_outputs))
+        kong.log.info("########################## llm_predictor ##########################")
+        File_object = open("/tmp/analytics/file.txt", "w+")
+        kong.log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  Prompt Query : ",kong.request.get_header("prompt"))
+        File_object.write(kong.request.get_raw_body().replace('\r', ''))
+        documents = SimpleDirectoryReader(directory_path).load_data()
+        kong.log.info("documents")
+
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+        kong.log.info("########################## service_context ##########################")
+        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        kong.log.info("########################## index ##########################")
+        index.save_to_disk('/tmp/file_index.json')
+        print("Saved to disk")
+
+
+        index = GPTSimpleVectorIndex.load_from_disk('/tmp/file_index.json')
+        query = kong.request.get_header("prompt")
+        response = index.query(query)
+        display(Markdown(f"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   Response: <b>{response.response}</b>"))
+        return kong.response.exit(200, {"message":response.response} )
+
+
+# add below section to allow this plugin optionally be running in a dedicated process
+if __name__ == "__main__":
+    from kong_pdk.cli import start_dedicated_server
+    start_dedicated_server("py-ai-analytics", Plugin, version, priority, Schema)
---
+++ README.md
@@ -0,0 +1,84 @@
+# AI-Powered-Analytics (AI Powered Python Kong Plugin)
+## Overview
+This is a AI Powered kong plugin developed using python which uses NLP and semantic analyzer to produce report form unformatted and ustructured data.
+It uses open API Key , NLP and Machine learning Algorithm to analyse the input data and produces human like response.
+
+## Tested in Kong Release
+Kong Enterprise 2.8.2.1
+
+## Installation
+### Install Kong Pdk and Python Packages 
+```
+$ apk update && apk add python3 py3-pip python3-dev musl-dev libffi-dev gcc g++ file make && PYTHONWARNINGS=ignore pip3 install kong-pdk
+$ pip install llama-index==0.5.6
+$ pip install langchain==0.0.148
+$ pip install ipython
+```
+### Make the Below Changes in Kong.conf
+
+```
+$ pluginserver_names=python
+$ pluginserver_python_socket=/usr/local/kong/python_pluginserver.sock
+$ pluginserver_python_start_cmd = /opt/kong-python-pdk/kong-pluginserver --no-lua-style --plugins-directory <PATH_OF_PLUGIN_FOLDER> -v
+$ pluginserver_python_query_cmd = /opt/kong-python-pdk/kong-pluginserver --no-lua-style --plugins-directory <PATH_OF_PLUGIN_FOLDER> --dump-all-plugins
+```
+After Installing the Plugin using any of the above steps . Add the Plugin Name in Kong.conf
+
+```
+plugins = bundled,AI-Powered-Analytics
+
+```
+### Restart Kong
+
+```
+kong restart
+
+```
+# Configuration Reference
+
+## Enable the plugin on a Route
+
+### Admin-API
+For example, configure this plugin on a service by making the following request:
+		
+	curl -X POST http://{HOST}:8001/routes/{ROUTE}/plugins \
+	--data "name=AI-Powered-Analytics"  \
+	--data "config.OPENAI_API_KEY={OPENAI_API_KEY}"
+	--data "config.model_name={MODEL_NAME}"
+
+### Declarative(YAML)
+For example, configure this plugin on a service by adding this section to your declarative configuration file:
+			
+	routes : 
+	 name: {ROUTE}
+	 plugins:
+	 - name: AI-Powered-Analytics
+	 config:
+	   plugin_names: {OPENAI_API_KEY}
+	   model_name : {MODEL_NAME}
+	 enabled: true
+	 protocols:
+	 - grpc
+	 - grpcs
+	 - http
+	 - https
+
+ROUTE is the id or name of the route that this plugin configuration will target.
+OPENAI_API_KEY is the openAI Key .
+MODEL_NAME is the AI Model name
+
+
+## Parameters
+
+| FORM PARAMETER	     														| DESCRIPTION										  													|
+| ----------- 																		| -----------																								|							|
+| ROUTE<br>Type:string   											|  The name of the Route  the plugin targets.									|
+| config.OPENAI_API_KEY<br>Type:string              |  Accepts the openAI Key |
+| config.MODEL_NAME<br>Type:string              |  AI Model Name|
+
+
+
+
+## Contributors
+Design & Developed By : Satyajit.Sial@VERIFONE.com ,Prema.Namasivayam@VERIFONE.com, dhaval.mavani@verifone.com <br>
+Guided By    : Vineet.Dutt@VERIFONE.com , krishna_p2@VERIFONE.com  
